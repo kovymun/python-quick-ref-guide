4060a0296b1b4e24a229e0b6df756c3781777b04
Kovy <codingstudent2021@gmail.com>
2024-12-17 18:09:58 +0200

    First Commit

---
+++ python-first-program/python-first-program.py
@@ -0,0 +1,7 @@
+# My first Python program
+# In this section, we will explore the entry points of a Python program and guide you through writing your first Python program.
+# Our first program will be the classic "Hello, World!"
+
+# A simple "Hello, World!" program in Python:
+
+print("Hello, World!")  # This line prints "Hello, World" to the console.
