d28650f60bd969f5292a168d689e6eaf68f7ee1a
Kovy <codingstudent2021@gmail.com>
2024-12-17 22:00:13 +0200

    Adding Python Variables directory

---
+++ python-variables/python-variables.py
@@ -0,0 +1,46 @@
+#PYTHON VARIABLES
+
+# Defining a variable in Python
+name = "James"  # This creates a variable called "name" and assigns it the value "James"
+print(name)  # This prints the value of 'name' to the console
+
+"""
+-----------------
+PYTHON VARIABLES:
+-----------------
+
+* A variable in Python is a named reference that stores data in memory.
+* SYNTAX: identifier_name = value (snake case - most used convention in Python)
+* EXAMPLE: first_name = "James"
+
+----------------------------
+VARIABLE NAMING CONVENTIONS:
+----------------------------
+
+1. Start with a letter or underscore (_): 
+   - Correct: user_name, _age
+   - Incorrect: 1user, -age
+
+2. Use only alphanumeric characters and underscores (A-Z, a-z, 0-9, _):
+   - Correct: user_age, account_balance
+   - Incorrect: user-age, account#balance
+
+3. Case-Sensitive:
+   - username and UserName are treated as different variables.
+
+4. Cannot be a reserved keyword:
+   - Avoid using keywords like def, class, if, while, etc.
+
+5. Readable and Descriptive:
+   - Use meaningful names like total_score, customer_name instead of x or y.
+
+---------------------------
+NAMING CONVENTION STANDARD:
+---------------------------
+
+1. Snake Case (preferred in Python):
+  - Words are separated by underscores.
+  - Example: first_name, last_name
+
+"""
+
