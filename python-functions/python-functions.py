a46bbe8a228d18fd752e474d2f8cbe8e2981760e
Kovy <codingstudent2021@gmail.com>
2024-12-18 16:18:17 +0200

    Adding Python Variables directory

---
+++ python-functions/python-functions.py
@@ -0,0 +1,22 @@
+# PYTHON FUNCTIONS
+
+# Syntax for a Python function
+def sum_numbers(a, b):
+    # function to return the sum of two numbers
+    return a + b
+
+print(sum_numbers(1, 2))
+
+"""
+-----------------
+PYTHON FUNCTIONS:
+-----------------
+
+* What is a function?
+  - A function is a block of reusable code that performs a specific task or action.
+
+* SYNTAX:
+    def function_name(parameters):
+    # Function body (logic)
+
+"""
