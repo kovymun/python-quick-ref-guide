1f084343c2d8d9fa439e4c9c134fdeea26149c62
Kovy <codingstudent2021@gmail.com>
2024-12-19 19:54:55 +0200

    Amending the content of the Python Functions file

--- python-functions/python-functions.py
+++ python-functions/python-functions.py
@@ -12,11 +12,17 @@ print(sum_numbers(1, 2))
 PYTHON FUNCTIONS:
 -----------------
 
-* What is a function?
-  - A function is a block of reusable code that performs a specific task or action.
+* What is a function?  
+  - A function is a block of reusable code designed to perform a specific task or action.  
+  - It helps avoid repetitive code by allowing the same function to be used multiple times in a program.  
+
+* Understanding a Function's Interface:  
+  - Arguments/Parameters: The input values passed into the function to customize its behavior.  
+  - Return Value: The result or output produced by the function after performing its task. 
 
 * SYNTAX:
     def function_name(parameters):
     # Function body (logic)
+    # Return value*
 
 """
