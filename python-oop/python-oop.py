a61584eebc0c783c242ebc13b85e9a1a64da5fac
Kovy <codingstudent2021@gmail.com>
2025-01-22 22:59:37 +0200

    Adding OOP Directory

---
+++ python-oop/python-oop.py
@@ -0,0 +1,17 @@
+# OBJECTED ORIENTATED PROGRAMMING IN PYTHON
+
+# CREATING A CLASS
+
+class FootballPlayers:
+  def __init__(self, name, nationality, goals_scored):
+    self.name = name
+    self.nationality = nationality
+    self.goals_scored = goals_scored
+
+  def __str__(self):
+    return f"Player: {self.name}, Nationality: {self.nationality}, Goals Scored: {self.goals_scored}"
+
+# OBJECT: INSTANTIATING AN INSTANCE OF A CLASS.
+
+player_calvin_williams = FootballPlayers("Calvin Williams", "South Africa", goals_scored=34)
+print(player_calvin_williams)
