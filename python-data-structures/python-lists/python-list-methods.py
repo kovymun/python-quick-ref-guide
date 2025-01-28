de4430026ffb755fd0b911206597d9a9cfd6f53d
Kovy <codingstudent2021@gmail.com>
2025-01-28 14:30:28 +0200

    Adding Python List Methods file

---
+++ python-data-structures/python-lists/python-list-methods.py
@@ -0,0 +1,21 @@
+# PYTHON LIST METHODS
+
+"""
+-------------------
+Python List Methods
+-------------------
+
+1. A list method is a built-in function that allows you to perform different/various actions on a list in Python.
+2. For example, list methods allow you to change, organize, or retrieve information from a list. Example: Adding, removing or sorting
+items in a list.
+3. Some of the most common list methods are: append(), extend(), pop(), remove(), and sort().
+
+"""
+
+# EXAMPLE: LIST APPEND METHOD
+# The append() method adds an item x to the end of the list. In this example, we are adding the "cart item: apple" to the "cart" list.
+cart = ["pears", "cactus"]
+cart.append("apple")
+
+print(cart) #Expected Output: ['pears', 'cactus', 'apple']
+
