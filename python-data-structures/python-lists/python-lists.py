7285845be5abe20c99ce61867b75411d5c1c53f8
Kovy <codingstudent2021@gmail.com>
2025-01-28 11:47:03 +0200

    Adding Python Lists Directory

---
+++ python-data-structures/python-lists/python-lists.py
@@ -0,0 +1,17 @@
+# PYTHON LIST
+
+"""
+-----------------------------
+Python Data Structures: Lists
+-----------------------------
+1. Python has 4 built-in data types used to store collections of data. This section will focus on "Lists".
+2. What is a List? A "List" is collection of items that can hold different elements.
+3. The list items are "ordered", "mutable", and can hold items of different data types (i.e. integer, or string)
+4. Defined using square brackets.
+5. SYNTAX: list_name = []
+
+"""
+
+# EXAMPLE: LIST HOLDING A COLLECTION OF FRUIT (OF TYPE STRING)
+fruits = ["Orange", "Strawberry", "Apple", "Peach"]
+print(fruits) #Expected Output: ['Orange', 'Strawberry', 'Apple', 'Peach']
