c6942584463071302e09c42347fce6cd3f6b9c60
Kovy <codingstudent2021@gmail.com>
2025-01-29 10:57:39 +0200

    Adding Python Data Types Directory

---
+++ python-data-types/python-data-types.py
@@ -0,0 +1,10 @@
+# PYTHON DATA TYPES
+
+"""
+PYTHON DATA TYPES
+
+1. Data stored in memory can be off many types
+2. 5 standard data types: numbers, strings, lists, tuples and dictionaries.
+3. type function -  Syntax: type(variable)
+4. float data type
+"""
