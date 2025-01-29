94aa0288361a586305750eea111c827689f2435e
Kovy <codingstudent2021@gmail.com>
2025-01-29 11:55:43 +0200

    Adding example to useful method file

--- python-useful-methods/python-useful-methods.py
+++ python-useful-methods/python-useful-methods.py
@@ -8,3 +8,8 @@ Python Useful Methods
 2. len(): The len() function returns the number of items in a data type/structure (like a list or string).
 3. sum(): The sum() function adds all elements in an iterable.
 """
+
+# EXAMPLE USING SUM() METHOD
+
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))  # Expected Output: 15
