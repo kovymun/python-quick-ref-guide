498c94ddddb75f9c8a52528db9c57cc5bad5fa4f
Kovy <codingstudent2021@gmail.com>
2025-01-29 11:39:11 +0200

    Adding Python Useful Methods Directory

---
+++ python-useful-methods/python-useful-methods.py
@@ -0,0 +1,10 @@
+# PYTHON USEFUL METHODS
+
+"""
+---------------------
+Python Useful Methods
+---------------------
+1. range(): The range() function creates a sequence of numbers, often used in loops.
+2. len(): The len() function returns the number of items in a data type/structure (like a list or string).
+3. sum(): The sum() function adds all elements in an iterable.
+"""
