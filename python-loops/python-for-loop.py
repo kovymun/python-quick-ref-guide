3d5db4e700635438fd80c279e75dbc4ec6ae282a
Kovy <codingstudent2021@gmail.com>
2025-01-29 11:17:40 +0200

    Adding the Python Loops directory

---
+++ python-loops/python-for-loop.py
@@ -0,0 +1,24 @@
+# PYTHON FOR LOOP
+
+"""
+----------------
+Python For loops
+----------------
+
+1. Loops are one of the most important concepts in any programming language.
+2. It repeats actions for each item in a sequence.
+3. BASIC SYNTAX:
+for variable in data-structure:
+    # Code logic to execute
+
+4. Using range() function:
+SYNTAX AND EXAMPLE:
+
+for num in range(1, 6):  # Loops from 1 to 5 (6 is excluded)
+    print(num)
+
+# Expected Output: 1 2 3 4 5
+
+5.
+
+"""
