08593370ee6ed4792c324b84b9d2d0aabeed0248
Kovy <codingstudent2021@gmail.com>
2025-01-31 12:32:29 +0200

    Adding Python Operators Directory

---
+++ python-operators/python-arithmetic-operators.py
@@ -0,0 +1,12 @@
+# PYTHON ARITHMETIC OPERATORS
+
+"""
+Python Arithmetic Operators:
+1. Addition (+) | Example a + b
+2. Subtraction (-) | Example a - b
+3. Multiplication (*) | Example a * b
+4. Division (/) | Example a / b
+5. Modulus (%): returns the remainder of a division operator | Example a % b
+6. Exponent (**): return an exponential value | Example a ** b
+7. Floor Division (//):
+"""
---
+++ python-operators/python-operators.py
@@ -0,0 +1,2 @@
+# PYTHON OPERATORS
+
