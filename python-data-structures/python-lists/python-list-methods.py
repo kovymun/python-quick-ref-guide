0715207676792573429c8ba6cfb77b4802d0fafd
Kovy <codingstudent2021@gmail.com>
2025-02-02 18:53:21 +0200

    Amending List Methods

--- python-data-structures/python-lists/python-list-methods.py
+++ python-data-structures/python-lists/python-list-methods.py
@@ -14,6 +14,7 @@ METHODS:
 * append(): SYNTAX: list.append(element). The append() method adds an item x to the end of the list.
 * pop(): SYNTAX: list.pop(index-position). The pop() method removes a list element at the specified position.
 If no index is provided, the last element of the list is removed and returned by default. This method also modifies the original list.
+* copy(): SYNTAX: list.copy(). The copy() method returns a copy of the specified list.
 
 """
 
@@ -22,5 +23,11 @@ If no index is provided, the last element of the list is removed and returned by
 cart = ["pears", "cactus"]
 cart.append("apple")
 
-print(cart) #Expected Output: ['pears', 'cactus', 'apple']
+print(cart) #Output: ['pears', 'cactus', 'apple']
+
+# Example: LIST COPY METHOD
+
+students = ["Tanya", "Giselle", "Esme"]
+student_copy = students.copy()
+print(student_copy) # Output: ['Tanya', 'Giselle', 'Esme']
 
