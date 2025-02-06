fbb96e05a9b6cdf7bb8a52d896d1e85ea3d819c3
Kovy <codingstudent2021@gmail.com>
2025-02-06 15:35:31 +0200

    Adding Python Operators Examples

--- python-operators/python-operators.py
+++ python-operators/python-operators.py
@@ -1,2 +1,25 @@
 # PYTHON OPERATORS
 
+"""
+--------------------
+Assignment operators
+--------------------
+1. Assignment operators are used to values to a variable. This values could be of type string, interger, even data structures like,
+lists or dictionaries.
+2. Some examples of assignment operators in python include:
+a) =
+b) += EXAMPLE num+=2 is the same as num = num +2
+"""
+
+# Example using Assignment operators
+# Defining a name variable
+name = "Kelvin Jones"
+
+# Initializing a numerical variable
+num = 1  # Initial number of parking tickets
+
+# Using an assignment operator to update the value (+=)
+num += 4  # Adds 4 to the existing value of num (1 + 4 = 5)
+
+print(f"{name} has {num} outstanding parking tickets") # Output: Kelvin Jones has 5 outstanding parking tickets.
+
