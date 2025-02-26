ceef6f90279612e19e3e267c2e62ba1f7ae6f331
Kovy <codingstudent2021@gmail.com>
2025-02-26 21:34:48 +0200

    Adding OOP inheritance notes

---
+++ python-oop/python-oop-pillar-inheritance.py
@@ -0,0 +1,13 @@
+# Object Orientated Programming: Inheritance
+
+"""
+What is inheritance?
+* Inheritance is one of the four fundamental pillars of Object-Oriented Programming (OOP), alongside Encapsulation, Abstraction,
+ Polymorphism. It allows a class (child class) to inherit attributes and methods from another class (parent class), promoting code
+ reusability, modularity, and hierarchical structuring.
+
+* It allows one class (child/subclass) to acquire properties and
+behaviors (attributes and methods) from another class (parent/superclass).
+
+* It promotes code reusability, modularity, and hierarchical structuring of code.
+"""
