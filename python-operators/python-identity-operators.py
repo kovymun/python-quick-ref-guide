532810b9f6a1005e67c3507c6b707f29990f6f81
Kovy <codingstudent2021@gmail.com>
2025-04-01 16:07:20 +0200

    Adding Python Identity Operators file

---
+++ python-operators/python-identity-operators.py
@@ -0,0 +1,21 @@
+# Python Identity Operators
+
+"""
+In this file we will explore the concept of IDENTITY OPERATORS in Python.
+
+1. In Python, identity operators (is and is not) are used to compare whether two variables refer to the same memory location.
+These are different from equality operators (== and !=), which check if the values of two variables are the same.
+
+2. The is operator:
+* The "is" operator checks if two variables point to the same object in memory.
+
+EXAMPLE:
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a is b)   # False (Different objects)
+print(a == b)   # True  (Same values)
+
+
+
+"""
