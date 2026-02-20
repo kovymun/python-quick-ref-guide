f7049b0b72541f0af6be9f57910421d59015a450
Koveshan Munsami <codingstudent2021@gmail.com>
2026-02-20 14:20:37 +0200

    docs: refactor guideline for list pop method

---
+++ python-data-structures/python-lists/python-list-methods/python-list-methods.md
@@ -0,0 +1,25 @@
+## PYTHON LIST METHODS
+
+### pop method
+
+1. The pop method removes a list element at a specific position (index).
+2. Another key point to remember, is that pop() does two things at once: it modifies the original list | and returns the
+   removed value.
+3. If no index is provided, the last element of the list is removed and returned by default.
+4. It also modifies the original list in memory.
+5. SYNTAX: `list.pop(index-position)`
+6. EXAMPLE:
+
+```
+numbers = [1, 2, 10]
+numbers.pop(1)  # Removes list element at index 1
+print(numbers)  # [1, 10]
+```
+
+---
+
+
+
+
+
+
--- python-data-structures/python-lists/python-list-methods/python-list-methods.py
+++ python-data-structures/python-lists/python-list-methods/python-list-methods.py
@@ -12,7 +12,7 @@ items in a list.
 
 METHODS:
 * append(): SYNTAX: list.append(element). The append() method adds an item x to the end of the list.
-* pop(): SYNTAX: list.pop(index-position). The pop() method removes a list element at the specified position.
+* pop(): SYNTAX: list.pop(index-position). The pop() method removes a list element at the specified position. It is also important to note is that pop() does two things at once — it modifies the original list and returns the removed value.
 If no index is provided, the last element of the list is removed and returned by default. This method also modifies the original list.
 * copy(): SYNTAX: list.copy(). The copy() method returns a copy of the specified list.
 
@@ -23,11 +23,10 @@ If no index is provided, the last element of the list is removed and returned by
 cart = ["pears", "cactus"]
 cart.append("apple")
 
-print(cart) #Output: ['pears', 'cactus', 'apple']
+print(cart)  # Output: ['pears', 'cactus', 'apple']
 
 # Example: LIST COPY METHOD
 
 students = ["Tanya", "Giselle", "Esme"]
 student_copy = students.copy()
-print(student_copy) # Output: ['Tanya', 'Giselle', 'Esme']
-
+print(student_copy)  # Output: ['Tanya', 'Giselle', 'Esme']
